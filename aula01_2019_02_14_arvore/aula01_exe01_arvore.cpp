46d85b76f97dfc4b48573f316152ced69984991a
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2019-02-23 17:51:43 -0300

    Adding class02

--- aula01_2019_02_14_arvore/aula01_exe01_arvore.cpp
+++ aula01_2019_02_14_arvore/aula01_exe01_arvore.cpp
@@ -13,15 +13,120 @@ struct cadastro {
 };
 typedef struct cadastro cad;
 
-//void inserir(cad *raiz) {}
+void inserir(cad *raiz) {
+	cad *novo = (cad *)malloc(sizeof(cad));
+
+	printf("Informe o valor: ");
+	scanf("%d", &novo -> valor);
+
+	novo -> prox = NULL;
+
+	if(raiz == NULL) {
+		raiz = novo;
+	}
+	else if(raiz -> ant == NULL && raiz < novo) {
+		raiz -> ant = novo;
+	}
+	else if(raiz -> prox == NULL) {
+		raiz -> prox = novo;
+	}
+
+	if(novo < raiz) {
+		cad *TEMP = raiz -> ant;
+
+		if(TEMP -> ant != NULL && novo < TEMP) {
+			TEMP = TEMP -> prox;
+
+			
+		}
+
+	}
+	else if(novo > raiz) {
+
+	}
+
+
+	else if(raiz -> ant != NULL && raiz < novo) {)
+		cad *TEMP = raiz -> ant;
+
+	else if(raiz -> prox ! NULL && raiz > novo) {
+		cad *TEMP = raiz -> ant;
+	}
+
+		if(TEMP -> ant == NULL && TEMP < raiz) {
+			TEMP -> ant == novo;
+		}
+		else if(TEMP -> prox == NULL && TEMP > raiz) {
+			TEMP -> prox == novo;
+		}
+
+		// while(TEMP -> ant != NULL) {
+		// 	TEMP = TEMP -> ant;
+		// }
+	}
+	
+
+}
+
+void menu(cad *raiz) {
+	int opcao, i=0;
+	
+	system("cls");
+	
+	while(i == 0) {
+		system("cls");
+		printf("::::::::::::::::::::::::::::::::::::::::::::::\n");
+		printf(":::::::::::::::::::. MENU .:::::::::::::::::::\n");
+		printf("::::::::::::::::::::::::::::::::::::::::::::::\n");
+		
+		printf("\n1 - Inserir");
+		printf("\n2 - Imprimir Todos");
+		printf("\n0 - Sair");
+		
+		printf("\n\nEscolha uma opcao: ");
+		scanf("%d", &opcao);
+		
+		switch(opcao) {
+			case 1 : {
+				//inserir fim
+				system("cls");
+				printf("::::::::::::::::::::::::::::::::::::::::::::::\n");
+				printf(":::::::::::::. INSERIR NO INICIO .::::::::::::\n");
+				printf("::::::::::::::::::::::::::::::::::::::::::::::\n\n");
+				
+				inserir(raiz);
+				break;
+			}
+			case 2 : {
+				//imprimir todos
+				system("cls");
+				printf("::::::::::::::::::::::::::::::::::::::::::::::\n");
+				printf("::::::::::::::. IMPRIMIR TODOS .::::::::::::::\n");
+				printf("::::::::::::::::::::::::::::::::::::::::::::::\n\n");
+				
+				show_all(raiz);
+				break;
+			}
+			case 0 : {
+				//sair
+				system("cls");
+				printf("::::::::::::::::::::::::::::::::::::::::::::::\n");
+				printf("::::::::::::::. SAINDO DO MENU .::::::::::::::\n");
+				printf("::::::::::::::::::::::::::::::::::::::::::::::\n\n");
+				printf("\tSAINDO...\n");
+								
+				exit(1);
+				break;
+			}
+		}
+	}
+	
+	system("pause");
+}
 
 int main() {
 	cad *raiz = (cad *)malloc(sizeof(cad));
 	raiz -> prox = NULL;
 	raiz -> ant = NULL;
-	
-	cad *folha = (cad *)malloc(sizeof(cad));
-	
-	
 }
 
