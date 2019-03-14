a7da2cc0ca1a2ac6ec1791422c2a39830a6af54a
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2019-03-13 23:35:59 -0300

    Adding class05

--- aula01_2019_02_14_arvore/aula01_exe01_arvore.cpp
+++ aula01_2019_02_14_arvore/aula01_exe01_arvore.cpp
@@ -13,6 +13,8 @@ struct cadastro {
 };
 typedef struct cadastro cad;
 
+//void imprimir(cad *raiz);
+
 void inserir(cad *raiz, int numero) {
 	cad *novo = (cad *)malloc(sizeof(cad));
 	
@@ -21,20 +23,20 @@ void inserir(cad *raiz, int numero) {
 	novo -> valor = NULL;
 	
 	if(raiz -> valor == NULL) {
-		printf("\ninserir raiz\n");
+//		printf("\ninserir raiz\n");
 //		printf("Informe o valor: ");
 //		scanf("%d", &raiz -> valor);
 		raiz -> valor = numero;
 		
-		printf("raiz %i\n\n", raiz -> valor);
+//		printf("raiz %i\n\n", raiz -> valor);
 	}
 	else {
-		printf("\ninserir novo\n");
+//		printf("\ninserir novo\n");
 //		printf("Informe o valor: ");
 //		scanf("%d", &novo -> valor);
 		novo -> valor = numero;
 		
-		printf("novo %i\n\n", novo -> valor);
+//		printf("novo %i\n\n", novo -> valor);
 	}
 	
 //	printf("OK-01\n\n");									//================== PRINT TEST ==================
@@ -44,7 +46,7 @@ void inserir(cad *raiz, int numero) {
 //	system("pause");
 
 	if(raiz -> ant == NULL && novo -> valor != NULL && novo -> valor < raiz -> valor) {
-		printf("novo ESQ %i\n", novo -> valor);
+//		printf("novo ESQ %i\n", novo -> valor);
 		raiz -> ant = novo;
 //		printf("OK-02\n\n");		//================== PRINT TEST ==================
 		
@@ -54,7 +56,7 @@ void inserir(cad *raiz, int numero) {
 //		system("pause");
 	}
 	else if(raiz -> prox == NULL && novo -> valor != NULL && novo -> valor > raiz -> valor) {
-		printf("novo DIR %i\n", novo -> valor);
+//		printf("novo DIR %i\n", novo -> valor);
 		raiz -> prox = novo;
 //		printf("OK-03\n\n");		//================== PRINT TEST ==================
 		
@@ -64,150 +66,117 @@ void inserir(cad *raiz, int numero) {
 //		system("pause");
 	}
 	else if(novo -> valor < raiz -> valor && raiz -> ant != NULL) {		//add on left to the tree
-		printf("LOOP OK-01\n\n");		//================== PRINT TEST ==================
-		system("pause");
+//		printf("LOOP OK-01\n\n");		//================== PRINT TEST ==================
+//		system("pause");
 		cad *TEMP = raiz -> ant;
-		printf("TEMP %i ", TEMP -> valor);
-		system("pause");
+//		printf("TEMP %i ", TEMP -> valor);
+//		system("pause");
 
 		while(novo -> valor < TEMP -> valor && TEMP -> ant != NULL) {
-			printf("LOOP OK-01-02\n\n");		//================== PRINT TEST ==================
-			system("pause");
-			printf("ANTES %i ", TEMP -> valor);
-			system("pause");
+//			printf("LOOP OK-01-02\n\n");		//================== PRINT TEST ==================
+//			system("pause");
+//			printf("ANTES %i ", TEMP -> valor);
+//			system("pause");
 			TEMP = TEMP -> ant;
-			printf("DEPOIS %i\n", TEMP -> valor);
-			system("pause");
+//			printf("DEPOIS %i\n", TEMP -> valor);
+//			system("pause");
 
 			while(novo -> valor > TEMP -> valor && TEMP -> prox != NULL) {
-				printf("LOOP OK-01-03\n\n");		//================== PRINT TEST ==================
-				system("pause");
-				printf("ANTES %i ", TEMP -> valor);
-				system("pause");
+//				printf("LOOP OK-01-03\n\n");		//================== PRINT TEST ==================
+//				system("pause");
+//				printf("ANTES %i ", TEMP -> valor);
+//				system("pause");
 				TEMP = TEMP -> prox;
-				printf("DEPOIS %i\n", TEMP -> valor);
-				system("pause");
+//				printf("DEPOIS %i\n", TEMP -> valor);
+//				system("pause");
 			}
 		}
 
 		if(TEMP -> ant == NULL) {
-			printf("IF OK\n\n");		//================== PRINT TEST ==================
-			system("pause");
-			printf("ANTES %i ", TEMP -> valor);
+//			printf("IF OK\n\n");		//================== PRINT TEST ==================
+//			system("pause");
+//			printf("ANTES %i ", TEMP -> valor);
 			TEMP -> ant = novo;
-			printf("DEPOIS %i\n", TEMP -> ant -> valor);
+//			printf("DEPOIS %i\n", TEMP -> ant -> valor);
 		}
 		else if(TEMP -> prox == NULL) {
-			printf("ELSE OK\n\n");		//================== PRINT TEST ==================
-			system("pause");
-			printf("ANTES %i ", TEMP -> valor);
-			system("pause");
+//			printf("ELSE OK\n\n");		//================== PRINT TEST ==================
+//			system("pause");
+//			printf("ANTES %i ", TEMP -> valor);
+//			system("pause");
 			TEMP -> prox = novo;
-			printf("DEPOIS %i\n", TEMP -> prox -> valor);
-			system("pause");
+//			printf("DEPOIS %i\n", TEMP -> prox -> valor);
+//			system("pause");
 		}
 	}
 	else if(novo -> valor > raiz -> valor && raiz -> prox != NULL) {		//add on right to the tree
-		printf("LOOP OK-02\n\n");		//================== PRINT TEST ==================
-		system("pause");
+//		printf("LOOP OK-02\n\n");		//================== PRINT TEST ==================
+//		system("pause");
 		cad *TEMP = raiz -> prox;
-		printf("TEMP %i ", TEMP -> valor);
-		system("pause");
+//		printf("TEMP %i ", TEMP -> valor);
+//		system("pause");
 
 		while(novo -> valor < TEMP -> valor && TEMP -> ant != NULL) {
-			printf("LOOP OK-02-02\n\n");		//================== PRINT TEST ==================
-			system("pause");
-			printf("ANTES %i ", TEMP -> valor);
-			system("pause");
+//			printf("LOOP OK-02-02\n\n");		//================== PRINT TEST ==================
+//			system("pause");
+//			printf("ANTES %i ", TEMP -> valor);
+//			system("pause");
 			TEMP = TEMP -> ant;
-			printf("DEPOIS %i\n", TEMP -> valor);
-			system("pause");
+//			printf("DEPOIS %i\n", TEMP -> valor);
+//			system("pause");
 
 			while(novo -> valor > TEMP -> valor && TEMP -> prox != NULL) {
-				printf("LOOP OK-02-03\n\n");		//================== PRINT TEST ==================
-				system("pause");
-				printf("ANTES %i ", TEMP -> valor);
-				system("pause");
+//				printf("LOOP OK-02-03\n\n");		//================== PRINT TEST ==================
+//				system("pause");
+//				printf("ANTES %i ", TEMP -> valor);
+//				system("pause");
 				TEMP = TEMP -> prox;
-				printf("DEPOIS %i\n", TEMP -> valor);
-				system("pause");
+//				printf("DEPOIS %i\n", TEMP -> valor);
+//				system("pause");
 			}
 		}
 
 		if(TEMP -> ant == NULL) {
-			printf("IF OK\n\n");		//================== PRINT TEST ==================
-			system("pause");
-			printf("ANTES %i ", TEMP -> valor);
+//			printf("IF OK\n\n");		//================== PRINT TEST ==================
+//			system("pause");
+//			printf("ANTES %i ", TEMP -> valor);
 			TEMP -> ant = novo;
-			printf("DEPOIS %i\n", TEMP -> ant -> valor);
+//			printf("DEPOIS %i\n", TEMP -> ant -> valor);
 		}
 		else if(TEMP -> prox == NULL) {
-			printf("ELSE OK\n\n");		//================== PRINT TEST ==================
-			system("pause");
-			printf("ANTES %i ", TEMP -> valor);
+//			printf("ELSE OK\n\n");		//================== PRINT TEST ==================
+//			system("pause");
+//			printf("ANTES %i ", TEMP -> valor);
 			TEMP -> prox = novo;
-			printf("DEPOIS %i\n", TEMP -> prox -> valor);
-			system("pause");
+//			printf("DEPOIS %i\n", TEMP -> prox -> valor);
+//			system("pause");
 		}
 	}
 	
-	system("pause");
+//	system("pause");
 }
 
 void show_all(cad *raiz) {
-	cad *TEMP1;
-	cad *TEMP2;
-	cad *TEMP3;
-	cad *TEMP4;
-	cad *TEMP5;
-	cad *TEMP6;
-	cad *TEMP7;
-	cad *TEMP8;
 	
-	TEMP1 = raiz -> ant -> ant;
-	TEMP2 = raiz -> ant;
-	TEMP3 = raiz -> ant -> prox;
-	TEMP4 = raiz;
-//	
-//	TEMP5 = raiz -> prox -> ant;
-//	TEMP6 = raiz -> prox;
-//	TEMP7 = raiz -> prox -> prox -> ant;
-//	TEMP8 = raiz -> prox -> prox;
-//	
-//	
-	printf("%i ", TEMP1 -> valor);
-	printf("%i ", TEMP2 -> valor);
-	printf("%i ", TEMP3 -> valor);
-	printf("%i ", TEMP4 -> valor);
-//	
-//	printf("%i ", TEMP5 -> valor);
-//	printf("%i ", TEMP6 -> valor);
-//	printf("%i ", TEMP7 -> valor);
-//	printf("%i ", TEMP8 -> valor);
+	cad *RAIZT = (cad *)malloc(sizeof(cad));
+	RAIZT = raiz;
 	
 		
-//	printf("%i ", TEMP2 -> valor);
-//	printf("%i ", TEMP4 -> valor);
-//	printf("%i ", TEMP6 -> valor);
+	if(RAIZT -> ant != NULL) {
+		show_all(RAIZT -> ant);
+	}
+	else {
+		printf("%i ", RAIZT -> valor);
+	}
 	
 	
-
-//	while(TEMP -> ant != NULL) {
-//		TEMP = TEMP -> ant;
-//		printf("%i", TEMP -> valor);
+	if(RAIZT -> prox != NULL) {
+		printf("%i ", RAIZT -> valor);
+		show_all(RAIZT -> prox);
+	}
+//	else {
+//		printf("%i ", RAIZT -> valor);
 //	}
-	
-//	if(TEMP -> ant == NULL) {
-//	printf("%i", TEMP -> valor);
-//	}
-	
-	
-	
-//	printf("raiz %i\n", raiz -> valor);					//================== PRINT TEST ==================
-//	printf("raiz ant %i\n", raiz -> ant -> valor);		//================== PRINT TEST ==================
-//	printf("raiz prox %i\n", raiz -> prox -> valor);	//================== PRINT TEST ==================
-	printf("\n");
-	system("pause");
 }
 
 void menu(cad *raiz) {
@@ -237,14 +206,21 @@ void menu(cad *raiz) {
 				printf("::::::::::::::::::::::::::::::::::::::::::::::\n\n");
 				
 //				inserir(raiz);
-				inserir(raiz, 14);
-				inserir(raiz, 10);
-				inserir(raiz, 6);
+				inserir(raiz, 5);
 				inserir(raiz, 2);
+				inserir(raiz, 1);
+				inserir(raiz, 3);
+				inserir(raiz, 4);
+				inserir(raiz, 8);
 				inserir(raiz, 7);
-				inserir(raiz, 12);
-				inserir(raiz, 30);
-				inserir(raiz, 28);
+				inserir(raiz, 6);
+				inserir(raiz, 9);
+				inserir(raiz, 10);
+//				inserir(raiz, 43);
+//				inserir(raiz, 32);
+//				inserir(raiz, 29);
+//				inserir(raiz, 36);
+//				inserir(raiz, 3);
 				break;
 			}
 			case 2 : {
@@ -255,6 +231,8 @@ void menu(cad *raiz) {
 				printf("::::::::::::::::::::::::::::::::::::::::::::::\n\n");
 				
 				show_all(raiz);
+				printf("\n\n");
+				system("pause");
 				break;
 			}
 			case 0 : {
